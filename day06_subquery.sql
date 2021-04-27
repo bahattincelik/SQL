dc81f0f2a3da60788db139b6640d97e447da9da0
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-04-27 21:44:14 +0300

    SQL 7

--- day06_subquery.sql
+++ day06_subquery.sql
@@ -1,5 +1,7 @@
 /* ============================= SUBQUERIES ====================================
+?
     SORGU içinde çal??an SORGUYA SUBQUERY (ALT SORGU) denilir.
+?
 ==============================================================================*/
 
     CREATE TABLE personel 
@@ -29,7 +31,7 @@
     INSERT INTO sirketler VALUES(101, 'Ford', 18000);
     INSERT INTO sirketler VALUES(102, 'Hyundai', 10000);
     INSERT INTO sirketler VALUES(103, 'Toyota', 21000);
-
+?
     SELECT *FROM sirketler;
     SELECT * FROM personel;
 /* -----------------------------------------------------------------------------
@@ -37,23 +39,77 @@
   sirkette calisan personelin isimlerini ve maa?lar?n? listeleyin
  -----------------------------------------------------------------------------*/ 
  
-  SELECT isim,maas,sirket FROM personel
-  WHERE sirket IN(SELECT sirket_adi FROM sirketler  
-                  WHERE personel_sayisi > 15000);
- 
- 
+    SELECT isim,maas,sirket FROM personel
+    WHERE sirket IN(SELECT sirket_adi FROM sirketler  
+                    WHERE personel_sayisi > 15000);
  /* ----------------------------------------------------------------------------
   ORNEK2: sirket_id’si 101’den büyük olan ?irket çal??anlar?n?n isim, maa? ve 
   ?ehirlerini listeleyiniz. 
- -----------------------------------------------------------------------------*/
- 
-/* -----------------------------------------------------------------------------
-  ORNEK3:  Ankara’da personeli olan ?irketlerin ?irket id'lerini ve personel 
-  say?lar?n? listeleyiniz 
- -----------------------------------------------------------------------------*/ 
- 
+  
+  -----------------------------------------------------------------------------*/
+   SELECT isim, maas, sehir FROM personel
+   WHERE sirket IN(SELECT sirket_adi FROM sirketler
+                   WHERE sirket_id > 101);
    
+   
+/* -----------------------------------------------------------------------------
+  ORNEK3: Ankara’da personeli olan ?irketlerin ?irket id'lerini ve personel 
+  say?lar?n? listeleyiniz.
+  
+  -----------------------------------------------------------------------------*/ 
+   SELECT sirket_id, personel_sayisi FROM sirketler
+   
+   WHERE sirket_adi IN (SELECT sirket FROM personel 
+                        WHERE sehir = 'Ankara');
+   
+  
 /* -----------------------------------------------------------------------------
   ORNEK4: Her ?irketin ismini, personel say?s?n? ve o ?irkete ait personelin
   toplam maa??n? listeleyen bir Sorgu yaz?n?z.
  -----------------------------------------------------------------------------*/
+ 
+      
+ /* ===================== AGGREGATE METOT KULLANIMI ===========================
+?
+    Aggregate Metotlar?(SUM,COUNT, MIN,MAX, AVG) Subquery içinde kullan?labilir.
+    Ancak, Sorgu tek bir de?er döndüryor olmal?d?r.
+?
+==============================================================================*/   
+    
+    SELECT sirket_adi, personel_sayisi, (SELECT SUM(maas) FROM personel
+                                      WHERE  sirket = sirketler.sirket_adi) AS toplam_maas
+                                     
+    FROM sirketler;
+ 
+/* -----------------------------------------------------------------------------
+  ORNEK5: Her ?irketin ismini, personel say?s?n? ve o ?irkete ait personelin
+  ortalama maa??n? listeleyen bir Sorgu yaz?n?z.
+ -----------------------------------------------------------------------------*/
+ 
+    SELECT sirket_adi, personel_sayisi, (SELECT ROUND(AVG(maas)) FROM personel
+                                        WHERE  sirket = sirketler.sirket_adi) AS ort_maas
+                                     
+    FROM sirketler;
+    
+ /* ----------------------------------------------------------------------------
+  ORNEK6: Her ?irketin ismini, personel say?s?n? ve o ?irkete ait personelin
+  maksimum ve minumum maa??n? listeleyen bir Sorgu yaz?n?z.
+ -----------------------------------------------------------------------------*/          
+    
+    SELECT sirket_adi, personel_sayisi, (SELECT MAX(maas) FROM personel WHERE sirket_adi=sirket) AS max_maas, 
+                                        (SELECT MIN (maas) FROM personel WHERE sirket_adi=sirket) AS min_maas
+                                       
+    FROM sirketler;
+    
+    
+       
+/* -----------------------------------------------------------------------------
+  ORNEK7: Her sirketin id’sini, ismini ve toplam kaç ?ehirde bulundu?unu 
+  listeleyen bir SORGU yaz?n?z.
+ -----------------------------------------------------------------------------*/
+    SELECT sirket_id, sirket_adi, ( SELECT COUNT(sehir) FROM personel
+                                    WHERE sirket = sirketler.sirket_adi) AS sehir_sayisi
+    
+    FROM sirketler;
+    
+    
