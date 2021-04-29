536231fc5aa1ad8494ae2e68e5e409a6d6b98a81
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-04-29 22:03:13 +0300

    SQL

--- day08_group_by.sql
+++ day08_group_by.sql
@@ -9,7 +9,7 @@
     fonksiyonlar? ile birlikte kullan?l?r.
     
 ==============================================================================*/ 
-      
+      DROP TABLE manav;
     CREATE TABLE manav 
     (
         isim varchar2(50), 
@@ -46,4 +46,97 @@
     SELECT urun_adi, COUNT(isim) AS kisi_sayisi
     FROM manav
     WHERE urun_adi IS NOT NULL
+    GROUP BY urun_adi;
+    
+    SELECT * FROM manav;
+    
+   
+/* ----------------------------------------------------------------------------
+  ORNEK3: sat?lan meyve türüne (urun_adi) göre sat?lan (urun_miktari )MIN ve 
+  MAX urun miktarlarini, MAX urun miktarina göre s?ralayarak listeyen sorguyu 
+  yaz?n?z.
+-----------------------------------------------------------------------------*/ 
+    SELECT urun_adi,MIN(urun_miktari) min_miktar, MAX(urun_miktari) maks_miktar
+    FROM manav
+    WHERE urun_adi IS NOT NULL
     GROUP BY urun_adi
+    ORDER BY MAX(urun_miktari) DESC;
+    
+/* ----------------------------------------------------------------------------
+  ORNEK4: kisi ismine ve urun ad?na göre sat?lan ürünlerin toplam?n? 
+  grupland?ran ve isime göre ters s?ras?da listeyen sorguyu yaz?n?z.
+ -----------------------------------------------------------------------------*/ 
+     SELECT isim, urun_adi, SUM(urun_miktari) as toplam_miktar
+     FROM manav
+     WHERE urun_adi IS NOT NULL
+     GROUP BY isim, urun_adi
+     ORDER BY isim DESC;
+     
+/* ----------------------------------------------------------------------------
+  ORNEK5: kisi ismine ve urun ad?na göre sat?lan ürünlerin toplam?n? bulan ve
+  ve bu toplam de?eri 3 ve fazlas? olan kay?tlar? toplam urun miktarlarina göre
+  ters siralayarak listeyen sorguyu yaz?n?z.
+ -----------------------------------------------------------------------------*/  
+     SELECT isim, urun_adi, SUM(urun_miktari) as toplam_miktar
+     FROM manav
+     GROUP BY isim, urun_adi
+     HAVING SUM(urun_miktari) >= 3
+     ORDER BY SUM(urun_miktari) DESC;
+     
+    -- AGGREGATE fonksiyonlar? ile ilgili bir ko?ul koymak için GROUP BY'dan 
+    -- sonra HAVING cümleci?i kullan?l?r.
+ 
+ /* ----------------------------------------------------------------------------
+  ORNEK6: sat?lan urun_adi'na göre MAX urun say?lar?n? s?ralayarak listeyen 
+  sorguyu yaz?n?z. NOT: Sorgu, sadece MAKS urun_miktari MIN urun_miktar?na 
+  e?it olmayan kay?tlar? listelemelidir.
+ -----------------------------------------------------------------------------*/    
+     SELECT urun_adi, MAX(urun_miktari)
+     FROM manav
+     GROUP BY urun_adi
+     HAVING MAX(urun_miktari) <> MIN(urun_miktari)
+     ORDER BY MAX(urun_miktari);
+
+
+ /*============================= DISTINCT =====================================
+    
+    DISTINCT cümleci?i bir SORGU ifadesinde benzer olan sat?rlar? flitrelemek
+    için kullan?l?r. Dolay?s?yla seçilen sutun yada sutunlar için benzersiz veri
+    içeren sat?rlar olu?turmaya yarar.
+   
+    SYNTAX
+    -------
+    SELECT DISTINCT sutun_adi1, sutun_adi2, satin_adi3
+    FROM  tablo_ad?;
+==============================================================================*/
+    
+     
+/* ----------------------------------------------------------------------------
+  ORNEK1: sat?lan farkl? meyve türlerinin say?s?n? listeyen sorguyu yaz?n?z.
+-----------------------------------------------------------------------------*/   
+    
+    SELECT COUNT(DISTINCT urun_adi) AS urun_sayisi 
+    FROM manav;
+
+/* -----------------------------------------------------------------------------
+  ORNEK2: sat?lan meyve ve isimlerin farkl? olanlar?n? listeyen sorguyu yaz?n?z.
+------------------------------------------------------------------------------*/
+     SELECT DISTINCT urun_adi,isim   -- 2 sutun icin beraber degerlendirirek
+                                     -- benzerli olanlar? cikariyor.
+     FROM manav; 
+     
+/* ----------------------------------------------------------------------------
+  ORNEK3: sat?lan meyvelerin urun_mikarlarinin benzersiz  olanlar?n?n 
+  toplamlarini listeyen sorguyu yaz?n?z.
+-----------------------------------------------------------------------------*/     
+     SELECT SUM(DISTINCT urun_miktari) AS urun_sayisi 
+     FROM manav;
+      
+    
+    
+    
+    
+    
+    
+    
+    
